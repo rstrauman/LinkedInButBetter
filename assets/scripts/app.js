c9d725038ef65e13b0347d83ba56802777de6265
daniel zhang <traceless0714@gmail.com>
2025-12-05 17:20:46 -0600

    add js stuff in, create new class client to store username and password, matching credentials in a for loop, event on login button and enter key to check cred, add comments for async/await because felt is not needed if data is hardcoded

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,108 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+const login = document.querySelector('.login');
+const wronginfo = document.querySelector('.wrong-info');
+const usernameinput = document.getElementById('username');
+const passwordinput = document.getElementById('password');
+const inputbox = document.querySelector('.input-box');
+usernameinput.value = '';
+passwordinput.value = '';
+let matched = false;
+
+
+class Client{
+  #username;
+  #password;
+
+  constructor(username, password) {
+    this.#username = username;
+    this.#password = password;
+  }
+
+  get username() {return this.#username};
+  get password() {return this.#password};
+}
+
+const user = [
+  new Client('daniel', 'daniel123'),
+  new Client('riley', 'riley123')
+];
+localStorage.setItem('user', JSON.stringify(user));
+
+function matchCred() {
+  const typedUser = usernameinput.value;
+  const typedPass = passwordinput.value;
+
+  for( let i = 0; i < user.length; i++) {
+    if (typedUser === user[i].username && typedPass === user[i].password) {
+      matched = true;
+      break;
+    } 
+  }
+}
+
+// function getUserData() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (!userOne) {
+//         reject(new Error('User does not exist'));
+//       } else {
+//         resolve(userOne);
+//       }
+//     }, 5000);
+//   });
+// }
+
+// function greetUser(user) {
+//   console.log(`Hello, ${user.username}`);
+// }
+
+// function errormsg(error) {
+//   console.log(error.message);
+// }
+
+// async function result() {
+//   try {
+//     const user = await getUserData();
+//     greetUser(user);
+//   } catch (error) {
+//     errormsg(error);
+//   } finally {
+//     console.log('nice to meet you');
+//   }
+// }
+
+// result();
+
+login.addEventListener('click', () => {
+  matchCred();
+  if(!matched) {
+    wronginfo.innerText = 'Incorrect username or password';
+    usernameinput.value = '';
+    passwordinput.value = '';
+  } else {
+    wronginfo.innerText = '';
+    window.location.href = './assets/pages/home.html';
+    usernameinput.value = '';
+    passwordinput.value = '';
+  }
+})
+inputbox.addEventListener('keydown', (e) => {
+  if(e.key === 'Enter'){
+    matchCred();
+    if(!matched) {
+      wronginfo.innerText = 'Incorrect username or password';
+      usernameinput.value = '';
+      passwordinput.value = '';
+    } else {
+      wronginfo.innerText = '';
+      window.location.href = './assets/pages/home.html';
+      usernameinput.value = '';
+      passwordinput.value = '';
+    }
+} 
+})
+
+})
